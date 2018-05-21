a81d6f99346aaaa96ecd9ffc75870d4da31d5a66
Sudheer Apte <sapte@rethinkrobotics.com>
2018-05-20 20:16:46 -0400

    added comment sections with copyright notice

--- machine.js
+++ machine.js
@@ -1,8 +1,20 @@
+/**
+   Copyright 2018 Sudheer Apte
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 /**
-   See README in the parent directory for an explanation of a state
-   machine. The full description is in "doc/machine.md".
-   
    This class represents one state machine tree. When you create an
    instance, it has only the root path "" and is in edit mode.
 
@@ -526,6 +538,5 @@ Machine.DPAT = /^D\s+([A-Za-z0-9.\/-]+)\s+(.*)/;
 Machine.DNULLPAT = /^D\s+([A-Za-z0-9.\/-]+)\s*/;
 Machine.APAT = /^A\s+([A-Za-z0-9.\/-]+)\s+(.*)/;
 Machine.DCONTPAT = /^C\s(.*)/;
-
 
 let machine = new Machine();
